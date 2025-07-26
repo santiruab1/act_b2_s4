865769a30d2ff8b69a938ea39d839a12e4bd5fb7
Santiago Rua <santiruab1@gmail.com>
2025-07-26 14:05:12 -0500

    Refactor UserRepository interface to separate it from UserService and fix typo in existsByEmail method

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,17 @@
 package com.example.demo.repository;
 
-public class UserRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByNameContainingIgnoreCase(String name);
+
+    List<User> findByEmailContainingIgnoreCase(String email);
+
+    boolean exiexistsByEmail(String email);
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,18 +1,6 @@
 package com.example.demo.service;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.User;
-
 public class UserService {
-    @Repository
-    public interface  UserRepository extends JpaRepository<User, Long> {
-        List<User> findByNameContainingIgnoreCase(String name);
-        List<User> findByEmailContainingIgnoreCase(String email);
-        boolean exiexistsByEmail(String email);        
-    }
+
 
 }
