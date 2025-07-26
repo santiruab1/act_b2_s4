f9a162d6ec9f6b1b830f301e6fbdf70f9b4ca0af
Santiago Rua <santiruab1@gmail.com>
2025-07-26 14:15:41 -0500

    Refactor UserService to interface and define user-related methods

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,25 @@
 package com.example.demo.service;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.model.User;
+
+public interface UserService {
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    User saveUser(User user);
+
+    User updateUser(Long id, User user);
+
+    void deleteUser(Long id);
+
+    List<User> findByName(String name);
+
+    List<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 
 }
